70cbdd5a0b271a0ba732dc89132c7d295c70793f
Luan <luan@riangle.com>
2021-04-06 20:51:13 +0200

    feat: test fonts

--- src/components/Pages/Page.js
+++ src/components/Pages/Page.js
@@ -50,12 +50,28 @@ const Page = ({
 			<link rel="shortcut icon" href={favIcon} />
 
 			<title>{title + " - WordPressify"}</title>
-			<link rel="preconnect" href="https://fonts.gstatic.com" />
 			<link
-				href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+				rel="preconnect"
+				href="https://fonts.gstatic.com"
+				crossOrigin="true"
+			/>
+			<link
 				rel="preload"
 				as="style"
+				href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
 			/>
+			<link
+				rel="stylesheet"
+				href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+				media="print"
+				onLoad="this.media='all'"
+			/>
+			<noscript>
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+				/>
+			</noscript>
 		</Head>
 		{children}
 	</React.Fragment>

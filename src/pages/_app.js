383a5aac0502c05acdf27eb5f927ce26f44f5b40
Luan Gjokaj <luan@riangle.com>
2021-04-09 15:49:32 +0200

    fix: font load

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import { useRouter, Router } from "next/router";
+import Head from "next/head";
 import NProgress from "nprogress";
 import { ThemeProvider } from "@emotion/react";
 import { Col, Container, MinHeight, Row } from "cherry-components";
@@ -21,6 +22,30 @@ function App({ Component, pageProps }) {
 			{globalStyles}
 			<ThemeProvider theme={theme}>
 				<ScrollToTop>
+					<Head>
+						<link
+							rel="preconnect"
+							href="https://fonts.gstatic.com"
+							crossOrigin="true"
+						/>
+						<link
+							rel="preload"
+							as="style"
+							href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+						/>
+						<link
+							rel="stylesheet"
+							href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+							media="print"
+							onLoad="this.media='all'"
+						/>
+						<noscript>
+							<link
+								rel="stylesheet"
+								href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+							/>
+						</noscript>
+					</Head>
 					<Header />
 					<MinHeight theme={theme}>
 						{router.pathname.includes("/docs") ? (

0e41f4b39643ad1dccc8533effb9fa6ee6b13db2
Luan <luan@riangle.com>
2021-06-11 16:18:52 +0200

    fix: github footer repo link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -35,7 +35,7 @@ function Footer({ className }) {
 					<Row gutterLg="default" className="first-row">
 						<Col xs={12} lg={6}>
 							<a
-								href="https://github.com/cherry-design-system/?ref=wordpressify.co"
+								href="https://github.com/luangjokaj/wordpressify/?ref=wordpressify.co"
 								target="_blank"
 								rel="noreferrer"
 								className="github-stars"
